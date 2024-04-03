e6fb7ab5ddbc25796604bfd14f6ddde8ca3932a2
pr0digious <137432323+pr0digious@users.noreply.github.com>
2024-04-03 11:27:36 -0700

    Update index.js
    Reverted the page back to its original form to restart with a blank canvas.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,7 @@
-import Navbar from "./components/Navbar"
-
 export default function Home() 
 {
 	return (
 		<main className='h-screen bg-grey-100 w-full'>
-			<Navbar/> 
 			<h1>PR Progress</h1>
 			<p>Track your progress through progressive overload.</p>
 		</main>
