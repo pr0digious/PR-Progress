b13d259a26e59e3034e6373b62336662ee1e0c81
pr0digious <137432323+pr0digious@users.noreply.github.com>
2024-04-03 11:26:41 -0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,4 @@
 import "@/styles/globals.css";
-import { useState } from 'react';
-import Navbar from "./components/Navbar"
-import Profile from "./Profile"
-import StartWorkout from "./StartWorkout"
-import Exercises from "./Exercises"
-import Home from "./index"
 
 
 export default function App({ Component, pageProps }) {
